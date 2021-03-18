52ac5b6ab9b63870c49c82eca81769a9a959a617
(hyebin) <(sksp4334@naver.com)>
2021-03-18 20:55:46 +0900

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-// import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
-// import Login from "./component/login/login";
-// import Home from "./component/home/home";
-// import Calendar from "./component/calendar/calendar";
+import Login from "./component/login/login";
+import Home from "./component/home/home";
+import Calendar from "./component/calendar/calendar";
 
 function App({ auth, repository }) {
   return (
-    <h2>Hello</h2>
-    // <BrowserRouter basename="/react-dietCalendar">
-    //   <Switch>
-    //     <Route exact path="/">
-    //       <Login auth={auth} repository={repository} />
-    //     </Route>
-    //     <Route path="/Home">
-    //       <Home auth={auth} />
-    //     </Route>
-    //     <Route path="/Calendar">
-    //       <Calendar repository={repository} auth={auth} />
-    //     </Route>
-    //   </Switch>
-    // </BrowserRouter>
+    <BrowserRouter basename="/react-dietCalendar">
+      <Switch>
+        <Route exact path="/">
+          <Login auth={auth} repository={repository} />
+        </Route>
+        <Route path="/Home">
+          <Home auth={auth} />
+        </Route>
+        <Route path="/Calendar">
+          <Calendar repository={repository} auth={auth} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
