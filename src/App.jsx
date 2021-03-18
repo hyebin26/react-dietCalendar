0d5f2a39bc7dd4090972bc8799b204b28df8c492
(hyebin) <(sksp4334@naver.com)>
2021-03-18 21:20:20 +0900

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./component/login/login";
 import Home from "./component/home/home";
@@ -6,19 +6,17 @@ import Calendar from "./component/calendar/calendar";
 
 function App({ auth, repository }) {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path="/">
-          <Login auth={auth} repository={repository} />
-        </Route>
-        <Route path="/Home">
-          <Home auth={auth} />
-        </Route>
-        <Route path="/Calendar">
-          <Calendar repository={repository} auth={auth} />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Login auth={auth} repository={repository} />
+      </Route>
+      <Route path="/Home">
+        <Home auth={auth} />
+      </Route>
+      <Route path="/Calendar">
+        <Calendar repository={repository} auth={auth} />
+      </Route>
+    </Switch>
   );
 }
 
