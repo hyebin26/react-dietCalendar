a19cc321aa913b12de0314bfdbeb6ac4fc32f4f2
(hyebin) <(sksp4334@naver.com)>
2021-03-20 16:52:06 +0900

    deploY@

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Login from "./component/login/login";
 import Home from "./component/home/home";
@@ -6,17 +6,19 @@ import Calendar from "./component/calendar/calendar";
 
 function App({ auth, repository }) {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Login auth={auth} repository={repository} />
-      </Route>
-      <Route path="/Home">
-        <Home auth={auth} />
-      </Route>
-      <Route path="/Calendar">
-        <Calendar repository={repository} auth={auth} />
-      </Route>
-    </Switch>
+    <BrowserRouter basename="/react-dietCalendar">
+      <Switch>
+        <Route exact path="/">
+          <Login auth={auth} repository={repository} />
+        </Route>
+        <Route path="/Home">
+          <Home auth={auth} />
+        </Route>
+        <Route path="/Calendar">
+          <Calendar repository={repository} auth={auth} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
